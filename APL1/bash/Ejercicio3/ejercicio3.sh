78cd698bd569e788a9f75d034f3c0ba3a7e15e94
Tomas Branchesi <tomasbeta@outlook.com>
2024-09-07 14:39:30 -0300

    added-validation-exercise3

---
+++ APL1/bash/Ejercicio3/ejercicio3.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#               Virtualizacion de Hardware              #
+#                                                       #
+#   APL1                                                #
+#   Nro ejercicio: 2                                    #
+#   Nro entrega: 1                                      #
+#   Nombre del script: ejercicio2.sh                    #
+#                                                       #
+#   Integrantes:                                        #
+#                                                       #
+#       Branchesi, Tomas Agustin      43013625          #
+#       Martucci, Federico Ariel      44690247          #
+#                                                       #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+declare ERROR_DIRECTORIO=1;
+
+function ayuda(){
+
+}
+
+
+
+
+if ! [ -d "$directorioEntrada" ]; then
+    echo "ERROR: Se debe especificar un directorio válido.";
+    exit $ERROR_DIRECTORIO;
+fi
+
